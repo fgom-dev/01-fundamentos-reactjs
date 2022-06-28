69ac892a23f1c4df4eb99c740f0de97816e40214
Fernando <fgom-dev@semog-pc.localdomain>
2022-06-27 22:51:56 -0300

    passando dados para o componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import style from './App.module.css'
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/fgom-dev.png',
+      name: 'Fernando Gomes',
+      role: 'Web Developer'
+    },
+    content :[
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'anchor', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-27 22:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/jorge-lba.png',
+      name: 'Jorge Alegretti',
+      role: 'Tech Lead'
+    },
+    content :[
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'anchor', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-25 18:00:00'),
+  },
+]
+
 export function App() {
   return ( 
     <div>
@@ -11,9 +42,15 @@ export function App() {
       <div className={style.wrapper}>      
         <Sidebar /> 
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>  
     </div> 
